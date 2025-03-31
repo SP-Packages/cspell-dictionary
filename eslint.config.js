f0d92291dc3646c399bbfd0807bb6ca97b911d50
Senthilprabu Ponnusamy <p.senthilprabu@gmail.com>
2025-03-31 00:59:10 -0400

    feat(package): add initial cspell-dictionary package

---
+++ eslint.config.js
@@ -0,0 +1,100 @@
+// Import the recommended ESLint configurations for JavaScript
+import globals from "globals";
+import js from "@eslint/js";
+
+import jsdocPlugin from "eslint-plugin-jsdoc";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default [
+  {
+    ignores: ["node_modules/", "dist/"],
+  },
+  // Use the recommended ESLint configurations
+  js.configs.recommended,
+  // Additions to the recommended ESLint configurations
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        wp: true,
+        jQuery: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      jsdocPlugin,
+      prettierPlugin,
+    },
+    rules: {
+      // ** ES6+ Best Practices **
+      "no-var": "error",
+      "prefer-const": "error",
+      "prefer-rest-params": "error",
+      "prefer-spread": "error",
+
+      // ** Functions **
+      "no-new-func": "error",
+      "no-param-reassign": "error",
+      "no-useless-call": "error",
+      "no-useless-catch": "error",
+      "no-useless-concat": "error",
+      "no-useless-return": "error",
+
+      // ** General Best Practices **
+      curly: ["error", "all"],
+      eqeqeq: ["error", "always"],
+      "no-console": "warn",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-magic-numbers": ["warn", { ignoreArrayIndexes: true, ignore: [0, 1] }],
+
+      // ** Stylistic Rules **
+      "arrow-spacing": "error",
+      camelcase: "error",
+      "comma-dangle": ["error", "never"],
+      "keyword-spacing": "error",
+      "no-irregular-whitespace": "error",
+      "no-mixed-spaces-and-tabs": "error",
+      "no-multi-spaces": "error",
+      "no-multiple-empty-lines": "error",
+      "no-tabs": "error",
+      "no-trailing-spaces": "error",
+      "no-whitespace-before-property": "error",
+      "object-curly-spacing": ["error", "always"],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "space-before-blocks": ["error", "always"],
+      "space-before-function-paren": "error",
+      "space-in-parens": ["error", "never"],
+      "space-infix-ops": "error",
+
+      // ** Variables **
+      "no-shadow": "error",
+      "no-shadow-restricted-names": "error",
+      "no-undef": "error",
+      "no-unused-vars": "warn",
+      "no-use-before-define": "error",
+
+      // **JSDoc**
+      "jsdocPlugin/require-jsdoc": "warn",
+      "jsdocPlugin/require-param": "warn",
+      "jsdocPlugin/require-returns": "warn",
+      "jsdocPlugin/check-alignment": "warn",
+      "jsdocPlugin/check-indentation": "warn",
+      "jsdocPlugin/require-description": "warn",
+
+      // **Prettier**
+      "prettierPlugin/prettier": "error",
+    },
+  },
+  eslintConfigPrettier,
+];
---
+++ release.config.js
@@ -0,0 +1,41 @@
+export default {
+  branches: ["main"],
+  repositoryUrl: "https://github.com/SP-Packages/cspell-dictionary.git",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          {
+            type: "build",
+            release: "patch",
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/exec",
+      {
+        // Regenerating package-lock.json
+        prepareCmd: "npm install --package-lock-only",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+        message: "chore(release): v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
